cf1671d8de9cdc7f8a435c6a5cf3f170be2d5830
ale_cande11 <candeago.alessandro@gmail.com>
2021-01-10 11:59:46 +0100

    Import requirements

---
+++ encription.py
@@ -0,0 +1,7 @@
+# AES 256 encryption/decryption using pycryptodome library
+
+from base64 import b64encode, b64decode
+import hashlib
+from Cryptodome.Cipher import AES
+import os
+from Cryptodome.Random import get_random_bytes
