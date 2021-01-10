cd0ba18bc9b201427315c5e7d465fa438a1a8263
ale_cande11 <candeago.alessandro@gmail.com>
2021-01-10 12:38:52 +0100

    decrypt function

--- decript.py
+++ decript.py
@@ -5,3 +5,23 @@ import os
 from Cryptodome.Random import get_random_bytes
 import getpass
 import json
+
+def decrypt(enc_dict, password):
+    # decode the dictionary entries from base64
+    salt = b64decode(enc_dict['salt'])
+    cipher_text = b64decode(enc_dict['cipher_text'])
+    nonce = b64decode(enc_dict['nonce'])
+    tag = b64decode(enc_dict['tag'])
+    
+
+    # generate the private key from the password and salt
+    private_key = hashlib.scrypt(
+        password.encode(), salt=salt, n=2**14, r=8, p=1, dklen=32)
+
+    # create the cipher config
+    cipher = AES.new(private_key, AES.MODE_GCM, nonce=nonce)
+
+    # decrypt the cipher text
+    decrypted = cipher.decrypt_and_verify(cipher_text, tag)
+
+    return decrypted
